0bd996f7b17b95052bd259d670326b29bb8df152
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-17 12:09:36 +0100

    Start parsing the XML file

--- bin/main.ml
+++ bin/main.ml
@@ -20,4 +20,11 @@ let _sample_xml : string =
 
 module XapiDb = Xapi_db.XapiDb
 
-let () = XapiDb.ping () |> print_endline
+let () =
+  XapiDb.ping () |> print_endline ;
+  (* We are expecting an XML file as input *)
+  if Array.length Sys.argv <> 2 then (
+    print_endline "Usage: xapi_db file.xml" ;
+    exit 1 ) ;
+  let _ = XapiDb.from_file Sys.argv.(1) in
+  print_endline "done"
--- lib/xapi_db.ml
+++ lib/xapi_db.ml
@@ -1,11 +1,48 @@
 module type Db = sig
-        type t
+  type t
 
-        val ping :unit -> string
+  val ping : unit -> string
+  (** [ping] is just use for testing *)
+
+  val from_file : string -> t
+  (** [from_file fname] reads XML from [fname] and build a relational database *)
 end
 
-module XapiDb: Db = struct
-        type t = unit
+module XapiDb : Db = struct
+  type t = unit
 
-        let ping () = "pong"
+  let ping () = "pong"
+
+  let from_file fname =
+    Printf.printf "Trying to read %s\n" fname ;
+    let ic = open_in fname in
+    let input = Xmlm.make_input (`Channel ic) in
+    let rec read_loop () =
+      try
+        (* input as a side effect *)
+        let () =
+          match Xmlm.input input with
+          | `Dtd name -> (
+            match name with
+            | None ->
+                print_endline "Dtd found"
+            | Some n ->
+                Printf.printf "Dtd %s found" n )
+          | `El_start (tag_name, _tag_attr_lst) ->
+              let uri, local = tag_name in
+              Printf.printf "Start uri: <%s>, local: <%s>\n" uri local
+          | `El_end ->
+              print_endline "End found"
+          | `Data data ->
+              Printf.printf "Data <%s> found\n" (data |> String.trim)
+        in
+        read_loop ()
+      with Xmlm.Error ((line, col), err) ->
+        if Xmlm.eoi input then print_endline "EOF reached"
+        else (
+          Printf.eprintf "[%d, %d]: Got exception: %s" line col
+            (Xmlm.error_message err) ;
+          exit 1 )
+    in
+    read_loop () ; In_channel.close ic
 end
