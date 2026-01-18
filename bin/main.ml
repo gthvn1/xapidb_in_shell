7183f0d8f979391395e6a0835e5c4a6f82f4f2ef
Guillaume <gthvn1@gmail.com>
2026-01-18 10:57:42 +0100

    Get remote parameters and multiple opaque ref

--- bin/main.ml
+++ bin/main.ml
@@ -1,34 +1,51 @@
 module XapiDb = Xapi_db.XapiDb
 
-type args = { fname : string; ref : string option }
-
-let usage =
-  {|
-Usage: xapi_db [OPTIONS] FILENAME [REF]
-
-Open an XAPI database file and optionally inspect an object reference.
-
-Arguments:
-  FILENAME    Path to the XAPI database file
-  REF         Opaque object reference (optional)
-
-Behavior:
-  If REF is not provided, xapi_db starts an interactive REPL.
-
-Options:
-  -h, --help  Show this help message and exit
-|}
+type args = {
+  fname : string;
+  refs : string list;
+  user : string option;
+  host : string option;
+}
 
 let get_args () =
-  match Sys.argv with
-  | [| _; "-h" |] | [| _; "--help" |] ->
-      print_string usage;
-      exit 0
-  | [| _; fname |] -> { fname; ref = None }
-  | [| _; fname; ref |] -> { fname; ref = Some ref }
-  | _ ->
-      print_string usage;
-      exit 1
+  let progname = Filename.basename Sys.argv.(0) in
+  let usage =
+    Printf.sprintf
+      "USAGE: %s [-user] [-host] <file> [<opaqueref1>] [<opaqueref2>] ... \n\n\
+       DESCRIPTION\n\
+       Open an XAPI database file and optionally inspect an object reference.\n\
+       If <opaqueref> is not provided, %s starts an interactive REPL.\n\n\
+       ARGUMENTS"
+      progname progname
+  in
+  let inputs = ref [] in
+  let user = ref "" in
+  let host = ref "" in
+
+  let anon_fun filename = inputs := filename :: !inputs in
+
+  let speclist =
+    [
+      ("-user", Arg.Set_string user, "Set username for remote access");
+      ("-host", Arg.Set_string host, "Set hostname or IP for remote access");
+    ]
+  in
+
+  let () = Arg.parse speclist anon_fun usage in
+  (* file is required *)
+  if List.length !inputs = 0 then (
+    print_endline "Error: file is missing";
+    Arg.usage speclist usage;
+    exit 1);
+
+  (* anon_fun builds inputs in reverse order, file is the last position *)
+  let inputs = List.rev !inputs in
+  {
+    fname = List.hd inputs;
+    refs = List.tl inputs;
+    user = (if String.length !user = 0 then None else Some !user);
+    host = (if String.length !host = 0 then None else Some !host);
+  }
 
 (* TODO: allow user host and remote as parameters and so we will be able to 
          call the XapiDb.from_channel remotely *)
@@ -49,13 +66,19 @@ let find_ref db ref =
 
 let () =
   let args = get_args () in
+
+  (* TODO: Manage remote connection with ssh cat *)
+  let _ = args.host in
+  let _ = args.user in
+
   (* We are expecting an XML file as input *)
   let ic = open_in args.fname in
   let db = XapiDb.from_channel ic in
   In_channel.close ic;
   Printf.printf "Found %d entries in DB\n" (XapiDb.size db);
 
-  match args.ref with
+  (* Todo: Read all refs *)
+  match List.nth_opt args.refs 0 with
   | None ->
       (* Let's try to find an ref in sample *)
       let ref_sample = "082d1948-c3f7-91ae-8793-568c9e888810" in
