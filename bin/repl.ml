aa41cdef6f76692430dddcb412f892d2e216abc5
Guillaume <gthvn1@gmail.com>
2026-01-19 21:14:38 +0100

    Add completions

--- bin/repl.ml
+++ bin/repl.ml
@@ -11,7 +11,7 @@ let help =
 |}
 
 module Cmd = struct
-  type cmd = Show of string | Follow of string | Back | Help | Quit
+  type cmd = Show of string | Follow of string | Back | Where | Help | Quit
 
   let from_string (s : string) : cmd option =
     let words s =
@@ -23,6 +23,7 @@ module Cmd = struct
     | [ "show"; ref ] -> Some (Show ref)
     | [ "follow"; ref ] -> Some (Follow ref)
     | [ "back" ] -> Some Back
+    | [ "where" ] -> Some Where
     | [ "help" ] -> Some Help
     | [ "exit" ] | [ "quit" ] -> Some Quit
     | _ -> None
@@ -32,6 +33,7 @@ module Cmd = struct
     | Show ref -> Helpers.print_ref db ref
     | Follow ref -> Printf.printf "TODO: follow %s\n" ref
     | Back -> Printf.printf "TODO: back\n"
+    | Where -> Printf.printf "TODO: where\n"
     | Help -> print_string help
     | Quit -> ()
 end
@@ -39,6 +41,17 @@ end
 let start (db : XapiDb.t) =
   LNoise.history_load ~filename:"history.txt" |> ignore;
   LNoise.history_set ~max_length:100 |> ignore;
+  (* Set completions for commands *)
+  LNoise.set_completion_callback (fun line_so_far comp ->
+      if line_so_far <> "" then
+        match line_so_far.[0] with
+        | 's' -> LNoise.add_completion comp "show"
+        | 'f' -> LNoise.add_completion comp "follow"
+        | 'b' -> LNoise.add_completion comp "back"
+        | 'w' -> LNoise.add_completion comp "where"
+        | 'h' -> LNoise.add_completion comp "help"
+        | 'q' -> LNoise.add_completion comp "quit"
+        | _ -> ());
   let rec loop () =
     match LNoise.linenoise "xapi_db> " with
     | None -> Printf.printf "Bye bye\n"
